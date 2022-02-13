8511f85ad52399f31d3dd2d2a46432ebb1ab5ef8
Antony <vontonnie@gmail.com>
2022-02-13 12:53:08 +0300

    Add Modifier.mediaQuery extension function

--- app/src/main/java/com/uxstate/screenmediaquery/ModifierExt.kt
+++ app/src/main/java/com/uxstate/screenmediaquery/ModifierExt.kt
@@ -1,2 +1,25 @@
 package com.uxstate.screenmediaquery
 
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import com.uxstate.DimensionsComparator
+
+
+fun Modifier.mediaQuery(comparator:DimensionsComparator, modifier: Modifier): Modifier = composed{
+
+
+    //context inside a composable =LocalContext.current
+    val screenWidth =
+        LocalContext.current.resources.displayMetrics.widthPixels.dp / LocalDensity.current.density
+
+    val screenHeight =
+        LocalContext.current.resources.displayMetrics.heightPixels.dp / LocalDensity.current.density
+
+     if (comparator.compare(screenWidth,screenHeight)){
+
+        this.then(modifier)
+    }else{this}
+}
