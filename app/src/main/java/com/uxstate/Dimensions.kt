6ef229da03788b0602a17571eca00350da81e6a5
Antony <vontonnie@gmail.com>
2022-02-13 12:32:42 +0300

    Add MediaQuery Composable

--- app/src/main/java/com/uxstate/Dimensions.kt
+++ app/src/main/java/com/uxstate/Dimensions.kt
@@ -1,6 +1,10 @@
 package com.uxstate
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 sealed class Dimensions {
 
@@ -8,9 +12,8 @@ sealed class Dimensions {
     object Height : Dimensions()
 
 
-
-
 }
+
 //how to compare values
 sealed class DimensionsOperator {
 
@@ -19,34 +22,57 @@ sealed class DimensionsOperator {
     object GreaterThan : DimensionsOperator()
 }
 
-    class DimensionsComparator(
-        val operator: DimensionsOperator,
-        private val dimension: Dimensions,
-        val value: Dp
+class DimensionsComparator(
+    val operator: DimensionsOperator,
+    private val dimension: Dimensions,
+    val value: Dp
 
 
-        /*Here we want to check the width(i.e. Dimension)
-        is less than (i.e. DimensionsOperator)
-        400.dp (i.e. the value)*/
-    ){
+    /*Here we want to check the width(i.e. Dimension)
+    is less than (i.e. DimensionsOperator)
+    400.dp (i.e. the value)*/
+) {
 
-        fun compare(screenWidth: Dp, screenHeight: Dp): Boolean {
+    fun compare(screenWidth: Dp, screenHeight: Dp): Boolean {
 
-            //check Dimen width
-            return if (dimension is Dimensions.Width){
-                when(operator){
-                    is Dimensions.DimensionsOperator.GreaterThan -> screenWidth > value
-                    is  Dimensions.DimensionsOperator.LessThan -> screenWidth <value
-                }
+        //check Dimen width
+        return if (dimension is Dimensions.Width) {
+            when (operator) {
+                is DimensionsOperator.GreaterThan -> screenWidth > value
+                is DimensionsOperator.LessThan -> screenWidth < value
+            }
 
-                //Else check height
-            }else{
+            //Else check height
+        } else {
 
-                when(operator){
+            when (operator) {
 
-                    is Dimensions.DimensionsOperator.GreaterThan -> screenHeight> value
-                    is Dimensions.DimensionsOperator.LessThan -> screenHeight<value
-                }
+                is DimensionsOperator.GreaterThan -> screenHeight > value
+                is DimensionsOperator.LessThan -> screenHeight < value
             }
         }
     }
+}
+
+@Composable
+fun MediaQuery(comparator: DimensionsComparator, content: @Composable () -> Unit) {
+
+    //context inside a composable =LocalContext.current
+    val screenWidth =
+        LocalContext.current.resources.displayMetrics.widthPixels.dp / LocalDensity.current.density
+
+    val screenHeight =
+        LocalContext.current.resources.displayMetrics.heightPixels.dp / LocalDensity.current.density
+
+    //show composable if compare() returns true
+
+    if (comparator.compare(screenWidth =screenWidth, screenHeight =screenHeight)) {
+
+        content()
+
+    }
+
+}
+
+
+infix fun Dimensions
