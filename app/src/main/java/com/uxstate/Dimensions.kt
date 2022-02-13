7063bbd2b6a3df3cd6e04bd4bc6d1f67ce0ec85f
Antony <vontonnie@gmail.com>
2022-02-13 11:01:45 +0300

    Add Dimensions Comparator Class

--- app/src/main/java/com/uxstate/Dimensions.kt
+++ app/src/main/java/com/uxstate/Dimensions.kt
@@ -1,7 +1,29 @@
 package com.uxstate
 
+import androidx.compose.ui.unit.Dp
+
 sealed class Dimensions {
 
-    object Width: Dimensions()
-    object Height: Dimensions()
+    object Width : Dimensions()
+    object Height : Dimensions()
+
+
+    //how to compare values
+    sealed class DimensionsOperator {
+
+
+        object LessThan : DimensionsOperator()
+        object GreaterThan : DimensionsOperator()
+    }
+
+    class DimensionsComparator(
+        val operator: DimensionsOperator,
+        private val dimension: Dimensions,
+        val value: Dp
+
+
+        /*Here we want to check the width(i.e. Dimension)
+        is less than (i.e. DimensionsOperator)
+        400.dp (i.e. the value)*/
+    )
 }
